436ffea8b6c3d0806ec3971dac0aa880d47df44f
natancunhadev <nncunha48@gmail.com>
2026-03-03 16:36:13 -0300

    3 commit - server connected, api, banco.

--- server/src/app.js
+++ server/src/app.js
@@ -3,13 +3,11 @@ const cors = require('cors')
 
 const app = express()
 
-// Middlewares globais
 app.use(cors())
 app.use(express.json())
 
-// Rota teste
 app.get('/', (req, res) => {
-  res.json({ message: 'Mama Connect API is running 💛' })
+  res.json({ message: 'Mama Connect running' })
 })
 
 module.exports = app
